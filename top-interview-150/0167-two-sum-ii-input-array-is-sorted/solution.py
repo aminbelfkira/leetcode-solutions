b3593c5ad5b5f95935d5e28179f9f61624a8ca7d
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-07-18 21:29:13 +0200

    solve: 167. Two Sum II - Input Array Is Sorted

---
+++ top-interview-150/0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -0,0 +1,20 @@
+# 167. Two Sum II - Input Array Is Sorted
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+# Accepted: 2026-07-18T19:29:09.000Z
+# Language: Python3
+# Collection: top-interview-150
+# Runtime: 3 ms · Beats 80.19%
+# Memory: 20.5 MB · Beats 36.22%
+# Submission: https://leetcode.com/submissions/detail/2072746769/
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for idx, num in enumerate(numbers) : 
+
+            if num in seen : 
+                return sorted([idx +1, seen[num] +1])
+            seen[target - num] = idx
+        
+        return 
