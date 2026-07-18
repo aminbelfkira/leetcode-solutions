0b3df8db7f600bd30c224f9481f06698561c6f53
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-07-18 21:59:27 +0200

    solve: 80. Remove Duplicates from Sorted Array II

--- top-interview-150/0080-remove-duplicates-from-sorted-array-ii/solution.py
+++ top-interview-150/0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -1,24 +1,23 @@
 # 80. Remove Duplicates from Sorted Array II
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
-# Accepted: 2026-07-18T19:54:19.000Z
+# Accepted: 2026-07-18T19:59:23.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 92 ms · Beats 30.48%
-# Memory: 21.8 MB · Beats 82.14%
-# Submission: https://leetcode.com/submissions/detail/2072763199/
+# Runtime: 91 ms · Beats 35.73%
+# Memory: 22.4 MB · Beats 24.89%
+# Submission: https://leetcode.com/submissions/detail/2072766230/
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        seens = {}
-        k = 0
-        for num in nums : 
-            
-            seen = seens.get(num,0)
 
-            if seen <= 1 : 
-                nums[k] = num
-                k+=1
-            
-            seens[num] = seen +1
+        n = len(nums)
+        if n <= 2 : 
+            return n
         
-        return k
+        i = 2
+        for k in range(2,n) : 
+            if nums[k] != nums[i-2] : 
+                nums[i] = nums[k]
+                i = i+1
+        
+        return i
