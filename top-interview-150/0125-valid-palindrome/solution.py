8e9dccd74c48222ede1744a1b945c61e78b16294
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-07-18 20:14:07 +0200

    solve: 125. Valid Palindrome

--- top-interview-150/0125-valid-palindrome/solution.py
+++ top-interview-150/0125-valid-palindrome/solution.py
@@ -1,24 +1,13 @@
 # 125. Valid Palindrome
 # https://leetcode.com/problems/valid-palindrome/
-# Accepted: 2026-07-18T17:06:14.000Z
+# Accepted: 2026-07-18T18:14:03.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 9 ms · Beats 41.88%
-# Memory: 23.4 MB · Beats 7.8%
-# Submission: https://leetcode.com/submissions/detail/2072597663/
+# Runtime: 7 ms · Beats 81.2%
+# Memory: 23.9 MB · Beats 5.25%
+# Submission: https://leetcode.com/submissions/detail/2072679845/
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [char.lower() for char in s if char.isalnum()]
-        n = len(s)
-        left, right = 0, n-1
-
-        while left < right : 
-
-            if s[left]== s[right] : 
-                left +=1
-                right -=1
-            else : 
-                return False
-
-        return True 
+        return s == s[::-1]
