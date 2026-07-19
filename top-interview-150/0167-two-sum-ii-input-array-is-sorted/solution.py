256a4da895ecaa93e305ad93870d03aa231ede6b
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-07-19 03:34:33 +0200

    solve: 167. Two Sum II - Input Array Is Sorted

--- top-interview-150/0167-two-sum-ii-input-array-is-sorted/solution.py
+++ top-interview-150/0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -1,11 +1,11 @@
 # 167. Two Sum II - Input Array Is Sorted
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-# Accepted: 2026-07-18T19:29:09.000Z
+# Accepted: 2026-07-19T01:34:29.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 3 ms · Beats 80.19%
-# Memory: 20.5 MB · Beats 36.22%
-# Submission: https://leetcode.com/submissions/detail/2072746769/
+# Runtime: 3 ms · Beats 80.22%
+# Memory: 20.5 MB · Beats 36.12%
+# Submission: https://leetcode.com/submissions/detail/2072874006/
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
