1c2402f33d964776ef79f5fac31b206720f61763
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-07-22 01:30:38 +0200

    solve: 88. Merge Sorted Array

---
+++ solutions/0088-merge-sorted-array/solution.py
@@ -0,0 +1,27 @@
+# 88. Merge Sorted Array
+# https://leetcode.com/problems/merge-sorted-array/
+# Accepted: 2026-07-21T23:30:35.000Z
+# Language: Python3
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.4 MB · Beats 37.62%
+# Submission: https://leetcode.com/submissions/detail/2076424971/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        i = m -1
+        j = n-1
+        k = m+n - 1
+
+        while j >=0: 
+
+            if i>=0 and nums1[i] > nums2[j] : 
+                nums1[k] = nums1[i]
+                i-=1
+            else : 
+                nums1[k] = nums2[j]
+                j-=1
+            k-=1
