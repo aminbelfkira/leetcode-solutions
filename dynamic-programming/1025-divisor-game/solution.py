8de1365e1ed223e0c39466db8c93c2d397757f7d
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-08-11 03:59:02 +0200

    solve: 1025. Divisor Game

---
+++ dynamic-programming/1025-divisor-game/solution.py
@@ -0,0 +1,15 @@
+# 1025. Divisor Game
+# https://leetcode.com/problems/divisor-game/
+# Accepted: 2026-08-11T01:58:55.000Z
+# Language: Python3
+# Collection: dynamic-programming
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.3 MB · Beats 44.7%
+# Submission: https://leetcode.com/submissions/detail/2102168063/
+
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        
+        return n%2 == 0
+
+        
