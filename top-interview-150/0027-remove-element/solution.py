2d0cc78b3c74034af320e1a5cdd40da9e854d82e
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-01 19:31:43 +0200

    solve: 27. Remove Element

--- top-interview-150/0027-remove-element/solution.py
+++ top-interview-150/0027-remove-element/solution.py
@@ -1,15 +1,14 @@
 # 27. Remove Element
 # https://leetcode.com/problems/remove-element/
-# Accepted: 2026-07-18T19:44:33.000Z
+# Accepted: 2026-09-01T17:31:39.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.2 MB · Beats 88.12%
-# Submission: https://leetcode.com/submissions/detail/2072757076/
+# Memory: 19.3 MB · Beats 57.58%
+# Submission: https://leetcode.com/submissions/detail/2127554199/
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
         k = 0
         for num in nums : 
             if num != val : 
