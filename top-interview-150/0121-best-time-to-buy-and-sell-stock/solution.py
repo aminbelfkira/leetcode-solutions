76a4040b772d185191a79f95b2a840b90b3fae92
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-01 20:15:45 +0200

    solve: 121. Best Time to Buy and Sell Stock

--- top-interview-150/0121-best-time-to-buy-and-sell-stock/solution.py
+++ top-interview-150/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,22 +1,19 @@
 # 121. Best Time to Buy and Sell Stock
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-# Accepted: 2026-07-18T20:17:40.000Z
+# Accepted: 2026-09-01T18:15:38.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 51 ms · Beats 51.68%
-# Memory: 28.5 MB · Beats 75.22%
-# Submission: https://leetcode.com/submissions/detail/2072776853/
+# Runtime: 62 ms · Beats 27.45%
+# Memory: 28.6 MB · Beats 76.32%
+# Submission: https://leetcode.com/submissions/detail/2127618879/
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        min_price = float(inf)
         max_profit = 0
+        min_price = float('inf')
 
         for price in prices : 
-
-            min_price = min(min_price, price)
+            min_price = min(price, min_price)
             max_profit = max(max_profit, price - min_price)
         
         return max_profit
-            
