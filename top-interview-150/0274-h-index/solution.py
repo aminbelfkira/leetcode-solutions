c874bc9e5192185ecc8370abcd09efded40d6884
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 12:45:29 +0200

    solve: 274. H-Index

--- top-interview-150/0274-h-index/solution.py
+++ top-interview-150/0274-h-index/solution.py
@@ -1,19 +1,20 @@
 # 274. H-Index
 # https://leetcode.com/problems/h-index/
-# Accepted: 2026-07-18T21:55:17.000Z
+# Accepted: 2026-09-03T10:45:24.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 67 ms · Beats 6.25%
-# Memory: 19.3 MB · Beats 64.25%
-# Submission: https://leetcode.com/submissions/detail/2072817634/
+# Runtime: 68 ms · Beats 6.64%
+# Memory: 19.4 MB · Beats 65.56%
+# Submission: https://leetcode.com/submissions/detail/2129512928/
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        def aux(citations, h) : 
-            return len([c for c in citations if c >=h]) >= h 
+        def aux(citations, k) : 
+
+            return len([c for c in citations if c>= k]) >=k
+
+        h = 0
+        while h < len(citations) and aux(citations, h+1) : 
+            h+=1
         
-        k = 0
-        while k<len(citations) and aux(citations, k+1) : 
-            k +=1
-    
-        return k
+        return h
