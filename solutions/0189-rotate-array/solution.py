484447eed3536f7b7193a13ea21a65d5c95bf5fe
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 12:28:15 +0200

    solve: 189. Rotate Array

--- solutions/0189-rotate-array/solution.py
+++ solutions/0189-rotate-array/solution.py
@@ -1,16 +1,16 @@
 # 189. Rotate Array
 # https://leetcode.com/problems/rotate-array/
-# Accepted: 2026-07-20T22:36:02.000Z
+# Accepted: 2026-09-03T10:28:10.000Z
 # Language: Python3
-# Runtime: 7 ms · Beats 53.7%
-# Memory: 26.7 MB · Beats 25.97%
-# Submission: https://leetcode.com/submissions/detail/2075119382/
+# Runtime: 30 ms · Beats 33.2%
+# Memory: 35.2 MB · Beats 14.6%
+# Submission: https://leetcode.com/submissions/detail/2129498243/
 
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, nums: list[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
         k = k%n
-        nums[:] = nums[n-k :] +nums[:n-k]
+        nums[:] = nums[n-k :] +nums[:n-k] 
