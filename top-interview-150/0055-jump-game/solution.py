f759d16213a5083f4d38f5fa5899a884d2bac40f
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 12:34:12 +0200

    solve: 55. Jump Game

--- top-interview-150/0055-jump-game/solution.py
+++ top-interview-150/0055-jump-game/solution.py
@@ -1,20 +1,21 @@
 # 55. Jump Game
 # https://leetcode.com/problems/jump-game/
-# Accepted: 2026-07-18T20:25:56.000Z
+# Accepted: 2026-09-03T10:34:07.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 22 ms · Beats 47.78%
-# Memory: 20.2 MB · Beats 85.1%
-# Submission: https://leetcode.com/submissions/detail/2072781239/
+# Runtime: 19 ms · Beats 60.32%
+# Memory: 20.4 MB · Beats 45.72%
+# Submission: https://leetcode.com/submissions/detail/2129503452/
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
-        current = 0
-        max_jump = nums[0] 
+        portee = 0 
+
+        for i in range(len(nums)-1) : 
+            
+            if i>portee : 
+                return False
+            portee = max(portee, i+ nums[i])
         
-        while current <len(nums) and current<= max_jump  : 
-            max_jump = max(max_jump, current + nums[current])
-            current +=1
-        
-        return max_jump >= len(nums) -1
+        return portee >= len(nums) -1
