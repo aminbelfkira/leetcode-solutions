b3044cc13609d87717b419982bc13038aab82b9c
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 22:50:18 +0200

    solve: 6. Zigzag Conversion

--- top-interview-150/0006-zigzag-conversion/solution.py
+++ top-interview-150/0006-zigzag-conversion/solution.py
@@ -1,23 +1,24 @@
 # 6. Zigzag Conversion
 # https://leetcode.com/problems/zigzag-conversion/
-# Accepted: 2026-07-19T00:14:12.000Z
+# Accepted: 2026-09-03T20:50:14.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 11 ms · Beats 54.06%
-# Memory: 19.5 MB · Beats 21.38%
-# Submission: https://leetcode.com/submissions/detail/2072853683/
+# Runtime: 7 ms · Beats 83.48%
+# Memory: 19.5 MB · Beats 19.27%
+# Submission: https://leetcode.com/submissions/detail/2130091177/
 
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows ==1 : 
+    def convert(self, s: str, num_rows: int) -> str:
+        if num_rows == 1 : 
             return s
-
         direction = -1
-        rows = [''] * numRows
+        rows = [''] *num_rows
         current_row = 0
+
         for char in s : 
             rows[current_row] += char
-            if current_row == 0 or current_row == numRows - 1:
-                direction = -direction  
+            if current_row == 0 or current_row == num_rows -1 : 
+                direction = - direction
             current_row += direction
-        return ''.join(rows)
+        return "".join(rows)
+
