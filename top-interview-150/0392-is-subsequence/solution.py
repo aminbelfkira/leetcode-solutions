da31426776fe991b87bf744b33333fb969f6fea9
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 23:03:29 +0200

    solve: 392. Is Subsequence

--- top-interview-150/0392-is-subsequence/solution.py
+++ top-interview-150/0392-is-subsequence/solution.py
@@ -1,22 +1,22 @@
 # 392. Is Subsequence
 # https://leetcode.com/problems/is-subsequence/
-# Accepted: 2026-07-19T01:34:16.000Z
+# Accepted: 2026-09-03T21:03:23.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 19 ms · Beats 2.89%
-# Memory: 69.7 MB · Beats 6.94%
-# Submission: https://leetcode.com/submissions/detail/2072873946/
+# Runtime: 23 ms · Beats 2.84%
+# Memory: 69.7 MB · Beats 7.6%
+# Submission: https://leetcode.com/submissions/detail/2130097362/
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         
-        if not s : 
+        if not s and not t : 
             return True
-        
-        if not t : 
+        elif not s : 
+            return True
+        elif not t : 
             return False
-        
-        if s[0] == t[0] : 
+        elif s[0] != t[0] : 
+            return self.isSubsequence(s, t[1:])
+        else : 
             return self.isSubsequence(s[1:], t[1:])
-        
-        return self.isSubsequence(s, t[1:])
