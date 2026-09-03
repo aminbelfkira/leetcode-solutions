d8fd9f5919422201ce22b3db8abd983cf0d4bcc2
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 22:39:26 +0200

    solve: 14. Longest Common Prefix

--- top-interview-150/0014-longest-common-prefix/solution.py
+++ top-interview-150/0014-longest-common-prefix/solution.py
@@ -1,20 +1,23 @@
 # 14. Longest Common Prefix
 # https://leetcode.com/problems/longest-common-prefix/
-# Accepted: 2026-07-18T23:38:25.000Z
+# Accepted: 2026-09-03T20:39:21.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 3 ms · Beats 20.43%
-# Memory: 19.3 MB · Beats 72.81%
-# Submission: https://leetcode.com/submissions/detail/2072844748/
+# Runtime: 4 ms · Beats 7.49%
+# Memory: 19.4 MB · Beats 7.25%
+# Submission: https://leetcode.com/submissions/detail/2130085848/
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
         if not strs : 
             return ""
-        
-        for i, chars in enumerate(zip(*strs)) : 
+        for i, chars in enumerate(zip(*strs)) :
+            print(chars)
             if len(set(chars)) >1 : 
                 return strs[0][:i]
-        
         return min(strs, key=len)
+                    
+
+        
+        
