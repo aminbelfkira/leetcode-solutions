fb04fa9cb619fe57a25421e2ce26cfb7ebafc633
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 22:35:50 +0200

    solve: 58. Length of Last Word

--- top-interview-150/0058-length-of-last-word/solution.py
+++ top-interview-150/0058-length-of-last-word/solution.py
@@ -1,13 +1,14 @@
 # 58. Length of Last Word
 # https://leetcode.com/problems/length-of-last-word/
-# Accepted: 2026-07-18T23:31:54.000Z
+# Accepted: 2026-09-03T20:35:46.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 52.19%
-# Submission: https://leetcode.com/submissions/detail/2072843059/
+# Memory: 19.3 MB · Beats 16.45%
+# Submission: https://leetcode.com/submissions/detail/2130083876/
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
+
         return len(s.split()[-1])
+        
