4a7edc322c11309eb32915c8153efff69086504f
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 22:42:39 +0200

    solve: 151. Reverse Words in a String

--- top-interview-150/0151-reverse-words-in-a-string/solution.py
+++ top-interview-150/0151-reverse-words-in-a-string/solution.py
@@ -1,15 +1,14 @@
 # 151. Reverse Words in a String
 # https://leetcode.com/problems/reverse-words-in-a-string/
-# Accepted: 2026-07-18T23:46:18.000Z
+# Accepted: 2026-09-03T20:42:34.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 39.93%
-# Submission: https://leetcode.com/submissions/detail/2072846781/
+# Memory: 19.4 MB · Beats 40.74%
+# Submission: https://leetcode.com/submissions/detail/2130087494/
 
 class Solution:
     def reverseWords(self, s: str) -> str:
         
-        s = s.split()[::-1]
-
-        return " ".join(s)
+        words = s.split()
+        return " ".join(words[::-1])
