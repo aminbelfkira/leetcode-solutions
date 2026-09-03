0b95fccfe1e8eaf955e152df6b0d4b8e8300c5a4
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-03 23:11:05 +0200

    solve: 11. Container With Most Water

--- top-interview-150/0011-container-with-most-water/solution.py
+++ top-interview-150/0011-container-with-most-water/solution.py
@@ -1,26 +1,26 @@
 # 11. Container With Most Water
 # https://leetcode.com/problems/container-with-most-water/
-# Accepted: 2026-07-19T01:39:09.000Z
+# Accepted: 2026-09-03T21:11:00.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 67 ms · Beats 10.83%
-# Memory: 29.6 MB · Beats 67.55%
-# Submission: https://leetcode.com/submissions/detail/2072875277/
+# Runtime: 57 ms · Beats 60.46%
+# Memory: 29.6 MB · Beats 38.37%
+# Submission: https://leetcode.com/submissions/detail/2130100633/
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        left, right = 0, len(height) -1
+        n = len(height)
+        left = 0
+        right = n-1
         max_area = 0
+        while left < right : 
+            min_height = min(height[left], height[right])
 
-        while left < right :
+            max_area = max(max_area, min_height * (right- left) )
 
-            width = right -left
-            current_area = min(height[left], height[right]) * width
-            max_area = max(max_area, current_area)
-            if height[left] < height[right] : 
-                left +=1
-            else : 
+            if height[left] > height[right] : 
                 right -=1
-            
+            else : 
+                left +=1
+        
         return max_area
