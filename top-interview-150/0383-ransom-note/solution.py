7721ca9e96ed8fea4f17054fcb2c4af3a322cefc
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-04 15:45:59 +0200

    solve: 383. Ransom Note

--- top-interview-150/0383-ransom-note/solution.py
+++ top-interview-150/0383-ransom-note/solution.py
@@ -1,15 +1,14 @@
 # 383. Ransom Note
 # https://leetcode.com/problems/ransom-note/
-# Accepted: 2026-07-20T22:48:45.000Z
+# Accepted: 2026-09-04T13:45:52.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 13 ms · Beats 82.19%
-# Memory: 19.7 MB · Beats 33.07%
-# Submission: https://leetcode.com/submissions/detail/2075123102/
+# Runtime: 19 ms · Beats 63.85%
+# Memory: 19.6 MB · Beats 73.05%
+# Submission: https://leetcode.com/submissions/detail/2130751608/
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
         from collections import Counter
 
-        return Counter(ransomNote)<= Counter(magazine)
+        return Counter(ransomNote) <= Counter(magazine)
