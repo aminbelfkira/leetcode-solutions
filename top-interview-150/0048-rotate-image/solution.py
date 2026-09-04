0352fe3872a863830216f195e3cde9483e435ca1
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-04 15:06:54 +0200

    solve: 48. Rotate Image

--- top-interview-150/0048-rotate-image/solution.py
+++ top-interview-150/0048-rotate-image/solution.py
@@ -1,24 +1,25 @@
 # 48. Rotate Image
 # https://leetcode.com/problems/rotate-image/
-# Accepted: 2026-07-19T14:06:39.000Z
+# Accepted: 2026-09-04T13:06:50.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.4 MB · Beats 27.84%
-# Submission: https://leetcode.com/submissions/detail/2073539160/
+# Memory: 19.2 MB · Beats 93.01%
+# Submission: https://leetcode.com/submissions/detail/2130715727/
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        n = len(matrix)
+        m = len(matrix)
+        n = len(matrix[0])
 
-        for i in range(n) : 
-            for j in range(i+1,n) : 
+        for i in range(m) : 
+            for j in range(i) : 
+
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        for i in range(n) : 
-            matrix[i] = matrix[i][::-1]
+        for i in range(m) : 
+            matrix[i][:] = matrix[i][::-1]
         
