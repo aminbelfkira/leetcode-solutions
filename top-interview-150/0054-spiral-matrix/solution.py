b3f1f1b60fe246b3b80d34a4cdd187c6dbbf9a6d
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-04 15:02:07 +0200

    solve: 54. Spiral Matrix

--- top-interview-150/0054-spiral-matrix/solution.py
+++ top-interview-150/0054-spiral-matrix/solution.py
@@ -1,41 +1,49 @@
 # 54. Spiral Matrix
 # https://leetcode.com/problems/spiral-matrix/
-# Accepted: 2026-07-19T12:57:16.000Z
+# Accepted: 2026-09-04T13:02:03.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 72.66%
-# Submission: https://leetcode.com/submissions/detail/2073474275/
+# Memory: 19.5 MB · Beats 7.76%
+# Submission: https://leetcode.com/submissions/detail/2130711427/
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
         m = len(matrix)
         n = len(matrix[0])
 
+        k = 0 
         left = 0
-        top =0
-        right = n-1
+        top = 0
         bottom = m-1
-        results = []
+        right = n-1
+        
+        res = []
 
+        while k < m*n : 
 
-        while top <= bottom and left <= right:
-
-            for i in range(left, right+1) : 
-                results.append(matrix[top][i])
-            top +=1
-
-            for i in range(top, bottom+1) : 
-                results.append(matrix[i][right])
+            for j in range(left, right +1) : 
+                res.append(matrix[top][j])
+                k+=1
+            top += 1
+            #print(top)
+            for i in range(top, bottom +1) : 
+                res.append(matrix[i][right]) 
+                k+=1
             right -=1
 
             if top <= bottom : 
-                for i in range(right, left-1 , -1) : 
-                    results.append(matrix[bottom][i])
+                for i in range(right, left -1, -1) : 
+                    res.append(matrix[bottom][i])
+                    k+=1
                 bottom -=1
             if left <= right : 
-                for i in range(bottom, top-1, -1) : 
-                    results.append(matrix[i][left])
+                for i in range(bottom, top-1 ,-1 ) : 
+                    res.append(matrix[i][left])
+                    k+=1
                 left +=1
+        return res
 
-        return results
+                
+
