2729c8a683bb27400494a13b1639c9afc4aecbc8
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 18:46:20 +0200

    solve: 1. Two Sum

--- top-interview-150/0001-two-sum/solution.py
+++ top-interview-150/0001-two-sum/solution.py
@@ -1,11 +1,11 @@
 # 1. Two Sum
 # https://leetcode.com/problems/two-sum/
-# Accepted: 2026-09-05T15:26:21.000Z
+# Accepted: 2026-09-05T16:46:15.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 20.8 MB · Beats 7.2%
-# Submission: https://leetcode.com/submissions/detail/2131862395/
+# Memory: 20.7 MB · Beats 7.2%
+# Submission: https://leetcode.com/submissions/detail/2131941582/
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
