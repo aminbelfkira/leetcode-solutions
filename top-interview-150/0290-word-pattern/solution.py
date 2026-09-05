3c72d029e3a3cc3349462be8f892e828cf672f2f
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 17:19:46 +0200

    solve: 290. Word Pattern

--- top-interview-150/0290-word-pattern/solution.py
+++ top-interview-150/0290-word-pattern/solution.py
@@ -1,32 +1,27 @@
 # 290. Word Pattern
 # https://leetcode.com/problems/word-pattern/
-# Accepted: 2026-07-20T23:17:59.000Z
+# Accepted: 2026-09-05T15:19:40.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 68.88%
-# Submission: https://leetcode.com/submissions/detail/2075130909/
+# Memory: 19.3 MB · Beats 71.38%
+# Submission: https://leetcode.com/submissions/detail/2131856307/
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split()
-        if len(pattern) != len(words) : 
-            return False
-        
-        n = len(pattern) 
-        char_to_word = {}
         word_to_char = {}
+        char_to_word = {}
 
-        for c, w in zip(pattern, words) : 
-            if c in char_to_word and char_to_word[c] != w : 
+        s = s.split()
+        print(s)
+        if len(pattern) != len(s) : 
+            return False
+        for i, (char, word) in enumerate(zip(pattern, s)) : 
+
+            if char in char_to_word and char_to_word[char] != word :
                 return False
-
-            if w in word_to_char and word_to_char[w] != c :
+            if word in word_to_char and word_to_char[word] != char : 
                 return False
-
-            word_to_char[w] = c 
-            char_to_word[c] = w 
-
-        return True  
-
-            
+            char_to_word[char] = word 
+            word_to_char[word] = char
+        return True
