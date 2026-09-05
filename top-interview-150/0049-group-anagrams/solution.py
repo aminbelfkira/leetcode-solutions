d6b849ffec4a40daae30afa39f9b254f81105480
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 17:24:24 +0200

    solve: 49. Group Anagrams

--- top-interview-150/0049-group-anagrams/solution.py
+++ top-interview-150/0049-group-anagrams/solution.py
@@ -1,20 +1,18 @@
 # 49. Group Anagrams
 # https://leetcode.com/problems/group-anagrams/
-# Accepted: 2026-07-20T23:26:43.000Z
+# Accepted: 2026-09-05T15:24:19.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 11 ms · Beats 82.98%
-# Memory: 22.1 MB · Beats 53.37%
-# Submission: https://leetcode.com/submissions/detail/2075133288/
+# Runtime: 19 ms · Beats 19.74%
+# Memory: 22.5 MB · Beats 41.29%
+# Submission: https://leetcode.com/submissions/detail/2131860598/
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         from collections import defaultdict
         anagrams = defaultdict(list)
 
         for word in strs : 
-            key = "".join(sorted(word))
+            key = str(sorted(word)) 
             anagrams[key].append(word)
-
         return list(anagrams.values())
