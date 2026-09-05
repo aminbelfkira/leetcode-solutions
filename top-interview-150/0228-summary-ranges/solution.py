58b74f96ce11a8dfb1ca954dc456e4d97ddeaf8e
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 23:45:54 +0200

    solve: 228. Summary Ranges

---
+++ top-interview-150/0228-summary-ranges/solution.py
@@ -0,0 +1,25 @@
+# 228. Summary Ranges
+# https://leetcode.com/problems/summary-ranges/
+# Accepted: 2026-09-05T21:45:50.000Z
+# Language: Python3
+# Collection: top-interview-150
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.3 MB · Beats 64.92%
+# Submission: https://leetcode.com/submissions/detail/2132176569/
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        k = 0
+        n = len(nums)
+        result = []
+        while k < n : 
+
+            start = k
+            while k+1 < n and nums[k+1] == nums[k] +1 :
+                k+=1
+            if start == k : 
+                result.append(str(nums[k]))
+            else : 
+                result.append(f"{nums[start]}->{nums[k]}")
+            k+=1
+        return result
