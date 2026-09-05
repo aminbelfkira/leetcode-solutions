245f385c80f9723e91e4727b70c41d49d8d728bf
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 23:35:34 +0200

    solve: 219. Contains Duplicate II

--- top-interview-150/0219-contains-duplicate-ii/solution.py
+++ top-interview-150/0219-contains-duplicate-ii/solution.py
@@ -1,21 +1,19 @@
 # 219. Contains Duplicate II
 # https://leetcode.com/problems/contains-duplicate-ii/
-# Accepted: 2026-07-20T23:55:27.000Z
+# Accepted: 2026-09-05T21:35:29.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 43 ms · Beats 19.24%
-# Memory: 39.5 MB · Beats 5.69%
-# Submission: https://leetcode.com/submissions/detail/2075141126/
+# Runtime: 47 ms · Beats 47.83%
+# Memory: 39.4 MB · Beats 21.95%
+# Submission: https://leetcode.com/submissions/detail/2132172955/
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        
-        seen = {}
+        indices = {}
 
-        for i, num in enumerate(nums) : 
+        for i,num in enumerate(nums) : 
 
-            if num in seen : 
-                if abs(seen[num] - i) <= k : 
-                    return True
-            seen[num] = i
+            if num in indices and abs(i - indices[num])<= k :
+                return True
+            indices[num] = i
         return False
