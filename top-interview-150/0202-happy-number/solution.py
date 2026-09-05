097c9bb893829c88a97da5753e0990bf1b70d3c9
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 18:50:18 +0200

    solve: 202. Happy Number

--- top-interview-150/0202-happy-number/solution.py
+++ top-interview-150/0202-happy-number/solution.py
@@ -1,22 +1,19 @@
 # 202. Happy Number
 # https://leetcode.com/problems/happy-number/
-# Accepted: 2026-07-20T23:50:50.000Z
+# Accepted: 2026-09-05T16:50:14.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.1 MB · Beats 90.18%
-# Submission: https://leetcode.com/submissions/detail/2075139828/
+# Memory: 19.1 MB · Beats 98.58%
+# Submission: https://leetcode.com/submissions/detail/2131946114/
 
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
 
-        while n!=1 : 
-
-            digits = [int(char)**2 for char in str(n)]
-            n = sum(digits)
+        while n !=1: 
+            n = sum([int(digit) **2 for digit in str(n)])
             if n in seen : 
                 return False
             seen.add(n)
-        
         return True
