276a50c420898f83aa82acb659577acddfa57865
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 13:27:08 +0200

    solve: 15. 3Sum

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -1,43 +1,36 @@
 # 15. 3Sum
 # https://leetcode.com/problems/3sum/
-# Accepted: 2026-07-20T22:02:43.000Z
+# Accepted: 2026-09-05T11:27:03.000Z
 # Language: Python3
-# Runtime: 402 ms · Beats 99.16%
-# Memory: 22.2 MB · Beats 53.7%
-# Submission: https://leetcode.com/submissions/detail/2075109337/
+# Runtime: 415 ms · Beats 98.57%
+# Memory: 22.3 MB · Beats 32.18%
+# Submission: https://leetcode.com/submissions/detail/2131648858/
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-
-        nums = sorted(nums)
         n = len(nums)
-        results = []
-
+        nums.sort()
+        res = []
         for i in range(n-2) : 
-
-            target = -nums[i]
-
-            if nums[i]>0 : 
-                break
-            
-            if i>0 and nums[i] == nums[i-1] : 
+            target = - nums[i]
+            if i>0 and nums[i] == nums[i-1] :
                 continue
+            if nums[i] >0 : 
+                break
             left = i+1
             right = n-1
             while left < right : 
-
-                current_sum =nums[left] + nums[right]
-                
-                if current_sum == target : 
-                    results.append([nums[i],nums[left], nums[right]])
+                current_sum = nums[left] + nums[right]
+                if current_sum ==target : 
+                    res.append([nums[i], nums[left], nums[right]])
                     left +=1
                     right -=1
-                    while left < right and nums[left] == nums[left -1] : 
+                    while left < right and nums[left] == nums[left-1] : 
                         left +=1
-                    while left < right and nums[right] == nums[right+1] : 
+                    while left < right and nums[right] == nums[right +1] : 
                         right -=1
                 elif current_sum > target : 
                     right -=1
-                else :
+                else : 
                     left +=1
-        return results
+        return res
