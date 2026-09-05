7608841241af616ba462e4be2631eee4a95510e9
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 17:26:26 +0200

    solve: 1. Two Sum

--- top-interview-150/0001-two-sum/solution.py
+++ top-interview-150/0001-two-sum/solution.py
@@ -1,21 +1,18 @@
 # 1. Two Sum
 # https://leetcode.com/problems/two-sum/
-# Accepted: 2026-07-20T23:34:55.000Z
+# Accepted: 2026-09-05T15:26:21.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 20.6 MB · Beats 6.98%
-# Submission: https://leetcode.com/submissions/detail/2075135530/
+# Memory: 20.8 MB · Beats 7.2%
+# Submission: https://leetcode.com/submissions/detail/2131862395/
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        seen = {}
+        counter = {}
 
         for i, num in enumerate(nums) : 
-
-            if num in seen : 
-                return [seen[num], i]
-            seen[target -num] = i
-         
-
+            if num in counter :
+                return [i,counter[num] ]
+            counter[target -num] = i
+        
