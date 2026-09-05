91dc550709b2034bc772f15ec5a80162e637f9ae
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-05 17:20:22 +0200

    solve: 242. Valid Anagram

--- top-interview-150/0242-valid-anagram/solution.py
+++ top-interview-150/0242-valid-anagram/solution.py
@@ -1,15 +1,14 @@
 # 242. Valid Anagram
 # https://leetcode.com/problems/valid-anagram/
-# Accepted: 2026-07-20T23:20:25.000Z
+# Accepted: 2026-09-05T15:20:17.000Z
 # Language: Python3
 # Collection: top-interview-150
-# Runtime: 3 ms · Beats 98.45%
-# Memory: 19.6 MB · Beats 25.99%
-# Submission: https://leetcode.com/submissions/detail/2075131570/
+# Runtime: 7 ms · Beats 91.71%
+# Memory: 19.4 MB · Beats 76.25%
+# Submission: https://leetcode.com/submissions/detail/2131856809/
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
         from collections import Counter
 
         return Counter(s) == Counter(t)
