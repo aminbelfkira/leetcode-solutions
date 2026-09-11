c98fd1bc7446affdf117859fe417b3d279d5c572
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-11 18:38:49 +0200

    solve: 45. Jump Game II

--- solutions/0045-jump-game-ii/solution.py
+++ solutions/0045-jump-game-ii/solution.py
@@ -1,22 +1,22 @@
 # 45. Jump Game II
 # https://leetcode.com/problems/jump-game-ii/
-# Accepted: 2026-09-04T12:29:33.000Z
+# Accepted: 2026-09-11T16:38:43.000Z
 # Language: Python3
-# Runtime: 10 ms · Beats 24.34%
-# Memory: 20 MB · Beats 65.9%
-# Submission: https://leetcode.com/submissions/detail/2130683858/
+# Runtime: 7 ms · Beats 51.48%
+# Memory: 20.3 MB · Beats 15.88%
+# Submission: https://leetcode.com/submissions/detail/2138754822/
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        
         current_end = 0
         portee = nums[0]
         jumps = 0
         n = len(nums)
+
         for i in range(n-1) : 
-            portee = max(portee, i+nums[i])
+            portee = max(portee, i + nums[i])
             if i == current_end : 
-                current_end = portee
                 jumps+=1
-            
+                current_end = portee
         return jumps
-            
