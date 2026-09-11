d36368245a5115fcfb79373e490343a4c752f73b
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-12 00:10:43 +0200

    solve: 289. Game of Life

--- solutions/0289-game-of-life/solution.py
+++ solutions/0289-game-of-life/solution.py
@@ -1,10 +1,10 @@
 # 289. Game of Life
 # https://leetcode.com/problems/game-of-life/
-# Accepted: 2026-07-20T22:33:55.000Z
+# Accepted: 2026-09-11T22:10:39.000Z
 # Language: Python3
-# Runtime: 4 ms · Beats 5.73%
-# Memory: 19.3 MB · Beats 47.33%
-# Submission: https://leetcode.com/submissions/detail/2075118762/
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.3 MB · Beats 49.54%
+# Submission: https://leetcode.com/submissions/detail/2138993348/
 
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
@@ -13,33 +13,26 @@ class Solution:
         """
         m = len(board)
         n = len(board[0])
+
         original = [row[:] for row in board]
 
-        def count_neighbors(board, i,j) : 
-            count = 0
-            for di in (-1,0,1) :
-                for dj in (-1,0,1) : 
-                    if di ==0 and dj ==0 : 
-                        continue
-                    
-                    ni = i+di
-                    nj = j+dj
-                    if 0<=ni<m and 0<=nj<n : 
-                        if board[ni][nj] : 
-                            count+=1 
-            return count
-        
-        for i in range(m) :
+        for i in range(m) : 
             for j in range(n) : 
-
-                alive = count_neighbors(original, i,j)
-                num_board = original[i][j]
-                if num_board and alive <2 : 
-                    board[i][j] = 0
-                elif num_board and alive in (2,3) : 
-                    continue
-                elif num_board and alive>3 : 
-                    board[i][j] = 0
-                elif num_board ==0 and alive ==3 : 
+                neighbors  =0 
+                
+                for di in (-1,0,1) :
+                    for dj in (-1,0,1) :
+                        ni = i+di
+                        nj = j+dj
+                        if 0<=ni<m and 0<=nj <n and not (i==ni and j==nj) : 
+                            neighbors += original[ni][nj]
+                if original[i][j] and neighbors < 2 : 
+                    board[i][j] =0
+                elif original[i][j] and neighbors in (2,3) : 
                     board[i][j] = 1
-        
+                elif original[i][j] and neighbors > 3 : 
+                    board[i][j] = 0
+                elif not original[i][j] and neighbors == 3 : 
+                    board[i][j] = 1
+                else : 
+                    board[i][j] = 0
