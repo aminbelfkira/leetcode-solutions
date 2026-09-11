c62a9258724329fa03d047dc6e8054ddd08c5cf0
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-12 00:13:02 +0200

    solve: 289. Game of Life

--- solutions/0289-game-of-life/solution.py
+++ solutions/0289-game-of-life/solution.py
@@ -1,36 +1,35 @@
 # 289. Game of Life
 # https://leetcode.com/problems/game-of-life/
-# Accepted: 2026-09-11T22:10:39.000Z
+# Accepted: 2026-09-11T22:11:18.000Z
 # Language: Python3
-# Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 49.54%
-# Submission: https://leetcode.com/submissions/detail/2138993348/
+# Runtime: 3 ms · Beats 16.72%
+# Memory: 19.4 MB · Beats 49.54%
+# Submission: https://leetcode.com/submissions/detail/2138993507/
 
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
+        original = [row[:] for row in board]
         m = len(board)
         n = len(board[0])
-
-        original = [row[:] for row in board]
-
         for i in range(m) : 
             for j in range(n) : 
-                neighbors  =0 
-                
-                for di in (-1,0,1) :
-                    for dj in (-1,0,1) :
-                        ni = i+di
-                        nj = j+dj
-                        if 0<=ni<m and 0<=nj <n and not (i==ni and j==nj) : 
+                neighbors = 0
+                for di in (-1, 0, 1) : 
+                    for dj in (-1, 0, 1) : 
+
+                        ni = i + di
+                        nj = j+ dj
+                        if 0<= ni <m and 0 <= nj < n and not(i==ni and j == nj):
                             neighbors += original[ni][nj]
-                if original[i][j] and neighbors < 2 : 
-                    board[i][j] =0
+                
+                if original[i][j] and neighbors <2 : 
+                    board[i][j] = 0
                 elif original[i][j] and neighbors in (2,3) : 
                     board[i][j] = 1
-                elif original[i][j] and neighbors > 3 : 
+                elif original[i][j] and neighbors >3 : 
                     board[i][j] = 0
                 elif not original[i][j] and neighbors == 3 : 
                     board[i][j] = 1
