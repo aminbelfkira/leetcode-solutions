a67617eb919a60fbc23258bb3eb200e8373545e2
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-11 23:11:15 +0200

    solve: 73. Set Matrix Zeroes

--- solutions/0073-set-matrix-zeroes/solution.py
+++ solutions/0073-set-matrix-zeroes/solution.py
@@ -1,29 +1,29 @@
 # 73. Set Matrix Zeroes
 # https://leetcode.com/problems/set-matrix-zeroes/
-# Accepted: 2026-07-20T22:23:08.000Z
+# Accepted: 2026-09-11T21:11:10.000Z
 # Language: Python3
-# Runtime: 7 ms · Beats 58.68%
-# Memory: 20.8 MB · Beats 15.02%
-# Submission: https://leetcode.com/submissions/detail/2075115675/
+# Runtime: 9 ms · Beats 27.26%
+# Memory: 20.7 MB · Beats 62.32%
+# Submission: https://leetcode.com/submissions/detail/2138973185/
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        #original = [row[:] for row in matrix]
 
-        m = len(matrix)
-        n =len(matrix[0])
         i_zeros = set()
         j_zeros = set()
-
-        for i in range(m) :
-            for j in range(n) : 
-                if matrix[i][j] == 0 : 
-                    i_zeros.add(i)
-                    j_zeros.add(j)
-
+        m = len(matrix)
+        n = len(matrix[0])
         for i in range(m) : 
             for j in range(n) : 
-                if i in i_zeros or j in j_zeros : 
-                    matrix[i][j] =0
+                if matrix[i][j] == 0 :
+                    i_zeros.add(i)
+                    j_zeros.add(j)
+        
+        for i in range(m) : 
+            for j in range(n) : 
+                if i in i_zeros or j in j_zeros: 
+                    matrix[i][j] = 0
