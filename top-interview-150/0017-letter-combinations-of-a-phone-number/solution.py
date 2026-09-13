9cc2425f4babc9ecb5b252d748c98f19483025ea
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-13 18:31:20 +0200

    solve: 17. Letter Combinations of a Phone Number

--- top-interview-150/0017-letter-combinations-of-a-phone-number/solution.py
+++ top-interview-150/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,11 +1,11 @@
 # 17. Letter Combinations of a Phone Number
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-# Accepted: 2026-09-13T16:29:20.000Z
+# Accepted: 2026-09-13T16:31:16.000Z
 # Language: Python3
 # Collection: top-interview-150
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.2 MB · Beats 95.84%
-# Submission: https://leetcode.com/submissions/detail/2140795578/
+# Memory: 19.3 MB · Beats 78.36%
+# Submission: https://leetcode.com/submissions/detail/2140797353/
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -21,7 +21,16 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-        combinations = ['']
-        for digit in digits : 
-            combinations = [prefix + char for prefix in combinations for char in letters[digit]]
-        return combinations 
+
+        result = []
+        path = []
+        def backtrack(index) : 
+            if index == len(digits) : 
+                result.append(''.join(path))
+                return
+            for char in letters[digits[index]] : 
+                path.append(char)
+                backtrack(index +1)
+                path.pop()
+        backtrack(0)
+        return result
