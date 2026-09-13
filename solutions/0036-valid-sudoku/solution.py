1f7b28fe1ac8c36382256c18dd7c9bb0678966d8
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-13 23:44:11 +0200

    solve: 36. Valid Sudoku

--- solutions/0036-valid-sudoku/solution.py
+++ solutions/0036-valid-sudoku/solution.py
@@ -1,30 +1,29 @@
 # 36. Valid Sudoku
 # https://leetcode.com/problems/valid-sudoku/
-# Accepted: 2026-07-21T23:26:06.000Z
+# Accepted: 2026-09-13T21:44:06.000Z
 # Language: Python3
-# Runtime: 5 ms · Beats 36.53%
-# Memory: 19.4 MB · Beats 9.53%
-# Submission: https://leetcode.com/submissions/detail/2076423744/
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.3 MB · Beats 74.62%
+# Submission: https://leetcode.com/submissions/detail/2141032900/
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        squares = [set() for _ in range(9)]
-
-        for i in range(9) : 
-            for j in range(9) : 
-
-                num = board[i][j]  
-                if num == "." : 
-                    continue
-                
-                num_square = 3*(i//3) + j//3
-                if num in rows[i] or num in cols[j] or num in squares[num_square] : 
-                    return False
-                rows[i].add(num)
-                cols[j].add(num)
-                squares[num_square].add(num)
         
+        row_set = [set() for _ in range(9)]
+        col_set = [set() for _ in range(9)]
+        square_set = [set() for _ in range(9)]
+        
+        for i in range(9) : 
+            for j in range(9) :
+                num = board[i][j]
+
+                if num == "." :
+                    continue
+                num_square = (i//3) * 3 + j //3
+                if num in row_set[i] or num in col_set[j] or num in square_set[num_square] : 
+                    return False
+                row_set[i].add(num)
+                col_set[j].add(num)
+                square_set[num_square].add(num)
+
         return True
