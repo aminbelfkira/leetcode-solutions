70e1dfaf4ac031f4fd6e5e68ff5fb0921e678b27
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-14 21:52:12 +0200

    solve: 399. Evaluate Division

---
+++ solutions/0399-evaluate-division/solution.py
@@ -0,0 +1,33 @@
+# 399. Evaluate Division
+# https://leetcode.com/problems/evaluate-division/
+# Accepted: 2026-09-14T19:52:08.000Z
+# Language: Python3
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.4 MB · Beats 75.68%
+# Submission: https://leetcode.com/submissions/detail/2141966892/
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        from collections import defaultdict, deque
+
+        graph = defaultdict(list)
+        for(a,b), value in zip(equations, values) : 
+            graph[a].append((b, value))
+            graph[b].append((a, 1.0/value))
+        
+        def evaluate(start, target) : 
+            if start not in graph or target not in graph :
+                return -1.0
+            queue = deque([(start, 1.0)])
+            visited = {start}
+
+            while queue :
+                current, ratio = queue.popleft()
+                if current == target : 
+                    return ratio
+                for neighbor, weight in graph[current] : 
+                    if neighbor not in visited : 
+                        visited.add(neighbor)
+                        queue.append((neighbor, ratio*weight))
+            return -1.0
+        return [evaluate(a,b) for a,b in queries]
