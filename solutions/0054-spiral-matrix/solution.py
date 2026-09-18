5d882ff919b8e997063c07fdbdacfc912692b31b
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 14:34:16 +0200

    solve: 54. Spiral Matrix

--- solutions/0054-spiral-matrix/solution.py
+++ solutions/0054-spiral-matrix/solution.py
@@ -1,23 +1,24 @@
 # 54. Spiral Matrix
 # https://leetcode.com/problems/spiral-matrix/
-# Accepted: 2026-09-13T19:29:15.000Z
+# Accepted: 2026-09-18T12:34:12.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.1 MB · Beats 99.3%
-# Submission: https://leetcode.com/submissions/detail/2140966055/
+# Memory: 19.5 MB · Beats 7.88%
+# Submission: https://leetcode.com/submissions/detail/2145727008/
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
         
-        m = len(matrix)
+        m = len(matrix) 
         n = len(matrix[0])
+        k = 0
         left = 0
         right = n-1
+        top = 0 
         bottom = m-1
-        top = 0
-        k = 0
         res = []
-        while k < m*n : 
+        while k < m*n :
+
             for j in range(left, right +1) : 
                 res.append(matrix[top][j])
                 k+=1
@@ -27,11 +28,12 @@ class Solution:
                 k+=1
             right -=1
 
-            if top <= bottom : 
-                for j in range(right, left -1, -1) :
+            if top <= bottom :
+                
+                for j in range(right, left -1, -1) : 
                     res.append(matrix[bottom][j])
                     k+=1
-                bottom-=1
+                bottom -=1
             if left <= right : 
                 for i in range(bottom, top -1, -1) : 
                     res.append(matrix[i][left])
