84f279fe96eb1c1eefaf2ef9efb0f7c640360e0b
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 15:39:05 +0200

    solve: 199. Binary Tree Right Side View

--- solutions/0199-binary-tree-right-side-view/solution.py
+++ solutions/0199-binary-tree-right-side-view/solution.py
@@ -1,10 +1,10 @@
 # 199. Binary Tree Right Side View
 # https://leetcode.com/problems/binary-tree-right-side-view/
-# Accepted: 2026-09-14T13:07:35.000Z
+# Accepted: 2026-09-18T13:39:01.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.4 MB · Beats 33.71%
-# Submission: https://leetcode.com/submissions/detail/2141575827/
+# Memory: 19.3 MB · Beats 33.9%
+# Submission: https://leetcode.com/submissions/detail/2145777381/
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,13 +13,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
-        result = []
-        level = [root] if root is not None else []
+    def rightSideView(self, root: TreeNode | None) -> list[int]:
+        level = [root] if root else []
+        res = []
 
         while level : 
-            result.append(level[-1].val) 
-            level = [c for child in level for c in (child.left, child.right) if c ]
-        
-        return result
+            res.append(level[-1].val)
+            level = [c for child in level for c in (child.left, child.right) if c is not None]
+        return res
