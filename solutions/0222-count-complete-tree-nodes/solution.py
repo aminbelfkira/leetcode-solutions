6ecc2f827a7bc0ca8434a41125dbd655a54a6ac5
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 15:03:10 +0200

    solve: 222. Count Complete Tree Nodes

--- solutions/0222-count-complete-tree-nodes/solution.py
+++ solutions/0222-count-complete-tree-nodes/solution.py
@@ -1,10 +1,10 @@
 # 222. Count Complete Tree Nodes
 # https://leetcode.com/problems/count-complete-tree-nodes/
-# Accepted: 2026-09-13T22:15:58.000Z
+# Accepted: 2026-09-18T13:03:06.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 24 MB · Beats 5.97%
-# Submission: https://leetcode.com/submissions/detail/2141043862/
+# Memory: 23.7 MB · Beats 53.86%
+# Submission: https://leetcode.com/submissions/detail/2145748182/
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,26 +13,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def countNodes(self, root: Optional[TreeNode]) -> int:
-        if not root : 
-            return 0
-        def get_left_height(node) : 
+    def countNodes(self, root: TreeNode | None) -> int:
+        
+        def get_left_height(node) :
             h = 0
-            while node :
+            while node : 
                 node = node.left
                 h+=1
             return h
         def get_right_height(node) : 
             h = 0
             while node : 
-                node = node.right 
+                node = node.right
                 h+=1
             return h
         
         left = get_left_height(root)
         right = get_right_height(root)
 
-        if left == right : 
+        if left == right :
             return 2**left -1
-        
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        else :
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
