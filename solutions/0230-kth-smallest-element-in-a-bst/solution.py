c2487659400bc7afb96c9c9a90e2517091aef84e
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 16:00:52 +0200

    solve: 230. Kth Smallest Element in a BST

--- solutions/0230-kth-smallest-element-in-a-bst/solution.py
+++ solutions/0230-kth-smallest-element-in-a-bst/solution.py
@@ -1,10 +1,10 @@
 # 230. Kth Smallest Element in a BST
 # https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-# Accepted: 2026-09-14T13:39:33.000Z
+# Accepted: 2026-09-18T14:00:47.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 22.1 MB · Beats 87.78%
-# Submission: https://leetcode.com/submissions/detail/2141604789/
+# Memory: 22.1 MB · Beats 87.98%
+# Submission: https://leetcode.com/submissions/detail/2145796144/
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,18 +13,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        i = 1 
+    def kthSmallest(self, root: TreeNode | None, k: int) -> int:
+        h = 0
         stack = []
-        node = root
+        current = root
 
-        while node is not None or stack :
-            while node is not None : 
-                stack.append(node) 
-                node = node.left
-            node = stack.pop()
-            if i == k:
-                return node.val
-            i+=1
-            node = node.right
-        return -1
+        while current is not None or stack : 
+            while current is not None : 
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            h+=1
+            if k == h : 
+                return current.val
+            current = current.right
+         
