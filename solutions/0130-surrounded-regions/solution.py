df9129039706560b5765ff3786c6dca7e2526fd1
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 16:37:29 +0200

    solve: 130. Surrounded Regions

--- solutions/0130-surrounded-regions/solution.py
+++ solutions/0130-surrounded-regions/solution.py
@@ -1,44 +1,39 @@
 # 130. Surrounded Regions
 # https://leetcode.com/problems/surrounded-regions/
-# Accepted: 2026-09-14T14:27:19.000Z
+# Accepted: 2026-09-18T14:37:25.000Z
 # Language: Python3
-# Runtime: 3 ms · Beats 84.42%
-# Memory: 22.3 MB · Beats 78.11%
-# Submission: https://leetcode.com/submissions/detail/2141650005/
+# Runtime: 0 ms · Beats 100%
+# Memory: 22.4 MB · Beats 60.39%
+# Submission: https://leetcode.com/submissions/detail/2145827037/
 
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def solve(self, board: list[list[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        m = len(board) 
+        m = len(board)
         n = len(board[0])
-
-        def dfs(r,c) :
-            if not (0<=r<m and 0 <=c<n) : 
-                return 
-            if board[r][c] == "O":
+        islands = 0
+        def dfs(r,c) : 
+            if not(0 <= r < m and 0 <= c < n) : 
+                return
+            if board[r][c] == "O" : 
                 board[r][c] = "#"
-                dfs(r+1, c)
-                dfs(r-1, c)
+                dfs(r+1,c)
+                dfs(r-1, c) 
                 dfs(r, c+1)
                 dfs(r, c-1)
-        for i in range(m) :
-            if board[i][0] == "O" : 
-                dfs(i,0)
-            if board[i][n-1] == "O" : 
-                dfs(i, n-1)
-        for j in range(n) : 
-            if board[0][j] == "O" :
-                dfs(0,j)
-            if board[m-1][j] == "O" : 
-                dfs(m-1,j)
-        for i in range(m) : 
-            for j in range(n) : 
-                if board[i][j] == '#' : 
-                    board[i][j] = "O"
-                elif board[i][j] =="O":
-                    board[i][j] = "X"
-                else : 
-                    continue
         
+        for i in range(m) : 
+            dfs(i, 0)
+            dfs(i, n-1)
+        for j in range(n) : 
+            dfs(0, j)
+            dfs(m-1, j)
+        
+        for r in range(m) : 
+            for c in range(n) : 
+                if board[r][c] == "O" : 
+                    board[r][c] = "X"
+                if board[r][c] == "#" : 
+                    board[r][c] = "O"
