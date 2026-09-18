bb5c0c2b4c02fb30671d757c48c4fd1354f9fc54
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-18 14:38:41 +0200

    solve: 92. Reverse Linked List II

--- solutions/0092-reverse-linked-list-ii/solution.py
+++ solutions/0092-reverse-linked-list-ii/solution.py
@@ -1,10 +1,10 @@
 # 92. Reverse Linked List II
 # https://leetcode.com/problems/reverse-linked-list-ii/
-# Accepted: 2026-09-13T19:38:37.000Z
+# Accepted: 2026-09-18T12:38:37.000Z
 # Language: Python3
-# Runtime: 4 ms · Beats 1.35%
-# Memory: 19.6 MB · Beats 25.64%
-# Submission: https://leetcode.com/submissions/detail/2140972276/
+# Runtime: 0 ms · Beats 100%
+# Memory: 19.3 MB · Beats 65.53%
+# Submission: https://leetcode.com/submissions/detail/2145730098/
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -12,20 +12,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head) 
+    def reverseBetween(self, head: ListNode | None, left: int, right: int) -> ListNode | None:
+        
+        dummy = ListNode(0, head)
         prev = dummy
-        current = head
-        for _ in range(left -1) :
+        for _ in range(left-1) : 
             prev = prev.next
-        curr = prev.next
-        print(prev)
-        print(curr)
-        for _ in range(right -left) : 
-            temp = curr.next
-            curr.next = temp.next
+        current = prev.next
+        for _ in range(right - left) : 
+            temp = current.next
+            current.next = temp.next
             temp.next = prev.next
             prev.next = temp
-
-        
+            
+            
         return dummy.next
