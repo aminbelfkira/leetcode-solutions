821ac6e19b8de5e0e1409399f0d23597cb6db8ae
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 09:42:05 +0200

    solve: 133. Clone Graph

--- solutions/0133-clone-graph/solution.py
+++ solutions/0133-clone-graph/solution.py
@@ -1,10 +1,10 @@
 # 133. Clone Graph
 # https://leetcode.com/problems/clone-graph/
-# Accepted: 2026-09-14T19:46:29.000Z
+# Accepted: 2026-09-21T07:42:01.000Z
 # Language: Python3
-# Runtime: 42 ms · Beats 91.87%
-# Memory: 19.7 MB · Beats 55.76%
-# Submission: https://leetcode.com/submissions/detail/2141963625/
+# Runtime: 44 ms · Beats 87.17%
+# Memory: 19.6 MB · Beats 98.09%
+# Submission: https://leetcode.com/submissions/detail/2148406351/
 
 """
 # Definition for a Node.
@@ -17,18 +17,16 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node is None :
+        if node is None : 
             return None
-        
         clones = {node : Node(node.val)}
         from collections import deque
         queue = deque([node])
         while queue : 
-            current = queue.popleft() 
+            current = queue.popleft()
             for neighbor in current.neighbors : 
                 if neighbor not in clones : 
                     clones[neighbor] = Node(neighbor.val)
                     queue.append(neighbor)
                 clones[current].neighbors.append(clones[neighbor])
         return clones[node]
-                
