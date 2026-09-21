910a057cc079e05e93c965ed3466473fd8591683
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 14:44:19 +0200

    solve: 35. Search Insert Position

--- solutions/0035-search-insert-position/solution.py
+++ solutions/0035-search-insert-position/solution.py
@@ -1,20 +1,21 @@
 # 35. Search Insert Position
 # https://leetcode.com/problems/search-insert-position/
-# Accepted: 2026-09-15T14:15:44.000Z
+# Accepted: 2026-09-21T12:44:13.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.9 MB · Beats 44.23%
-# Submission: https://leetcode.com/submissions/detail/2142672822/
+# Memory: 20 MB · Beats 44.89%
+# Submission: https://leetcode.com/submissions/detail/2148633592/
 
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
+        n = len(nums)
         left = 0
-        right = len(nums)
-        while left < right : 
+        right = n
+        while left < right :
             mid = (left + right)//2
 
-            if nums[mid]<target :
+            if nums[mid] < target : 
                 left = mid +1
             else : 
                 right = mid
-        return left
+        return left 
