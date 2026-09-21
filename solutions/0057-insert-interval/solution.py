fca7e427db9939e2d044bec6caf5006fc5f1f810
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 09:38:33 +0200

    solve: 57. Insert Interval

--- solutions/0057-insert-interval/solution.py
+++ solutions/0057-insert-interval/solution.py
@@ -1,31 +1,30 @@
 # 57. Insert Interval
 # https://leetcode.com/problems/insert-interval/
-# Accepted: 2026-09-11T17:09:03.000Z
+# Accepted: 2026-09-21T07:38:28.000Z
 # Language: Python3
-# Runtime: 0 ms · Beats 100%
-# Memory: 21.2 MB · Beats 89.25%
-# Submission: https://leetcode.com/submissions/detail/2138787746/
+# Runtime: 3 ms · Beats 30.12%
+# Memory: 21.3 MB · Beats 58.46%
+# Submission: https://leetcode.com/submissions/detail/2148404124/
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals)
-        ### on insert tout ceux avant 
+    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        ##on commence par les intervalles avatn celui à insérer
+
         new_start, new_end = newInterval
         k = 0
         res = []
-        while k <n and intervals[k][1] < new_start :
-            res.append(intervals[k])
-            k+=1
-        start = new_start
-        end = new_end
-        while k< n and intervals[k][0] <= new_end : 
-            start = min(start, intervals[k][0])
-            end  = max(end, intervals[k][1])
-            k+=1
-        res.append([start, end])
-
-        while k < n : 
+        while k<len(intervals) and intervals[k][1] < new_start : 
             res.append(intervals[k])
             k+=1
         
+        while k < len(intervals) and  intervals[k][0] <= new_end : 
+            new_start = min(new_start, intervals[k][0])
+            new_end = max(new_end, intervals[k][1])
+            k+=1
+        res.append([new_start, new_end])
+
+        while k < len(intervals) :
+            res.append(intervals[k])
+            k+=1
         return res
+        
