68f1e520e32699206bd19e7463a440134ff51ffc
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 13:39:49 +0200

    solve: 53. Maximum Subarray

--- solutions/0053-maximum-subarray/solution.py
+++ solutions/0053-maximum-subarray/solution.py
@@ -1,16 +1,17 @@
 # 53. Maximum Subarray
 # https://leetcode.com/problems/maximum-subarray/
-# Accepted: 2026-09-14T22:05:44.000Z
+# Accepted: 2026-09-21T11:39:44.000Z
 # Language: Python3
-# Runtime: 30 ms · Beats 75.01%
-# Memory: 31.4 MB · Beats 45.11%
-# Submission: https://leetcode.com/submissions/detail/2142025880/
+# Runtime: 39 ms · Beats 47.24%
+# Memory: 31.3 MB · Beats 75.09%
+# Submission: https://leetcode.com/submissions/detail/2148587102/
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        current = -float('inf')
-        best = -float('inf')
-        for num in nums :
-            current = max(num, current + num)
-            best = max(best, current)
-        return best
+    def maxSubArray(self, nums: list[int]) -> int:
+        
+        current_sum= 0
+        max_sum = -float('inf')
+        for num in nums : 
+            current_sum = max(current_sum + num, num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
