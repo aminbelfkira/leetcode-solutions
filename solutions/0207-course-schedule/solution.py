9b405e324880959feb67d2075b69866d5205d160
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 09:52:20 +0200

    solve: 207. Course Schedule

--- solutions/0207-course-schedule/solution.py
+++ solutions/0207-course-schedule/solution.py
@@ -1,27 +1,29 @@
 # 207. Course Schedule
 # https://leetcode.com/problems/course-schedule/
-# Accepted: 2026-09-14T19:56:30.000Z
+# Accepted: 2026-09-21T07:52:15.000Z
 # Language: Python3
-# Runtime: 6 ms · Beats 49.27%
-# Memory: 20.3 MB · Beats 78.99%
-# Submission: https://leetcode.com/submissions/detail/2141969508/
+# Runtime: 3 ms · Beats 85.82%
+# Memory: 20.2 MB · Beats 93.4%
+# Submission: https://leetcode.com/submissions/detail/2148412526/
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        from collections import defaultdict 
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        from collections import defaultdict
         graph = defaultdict(list)
-        indegree= [0] * numCourses
+        indegree = [0] * numCourses
         for course, prereq in prerequisites : 
             graph[prereq].append(course)
-            indegree[course]+=1
-        
-        queue = deque(c for c in range(numCourses) if indegree[c] == 0)
+            indegree[course] +=1
         visited = 0
-        while queue :
-            node= queue.popleft()
+        queue = deque(c for c in range(numCourses) if indegree[c] == 0 )
+
+        while queue : 
+            node = queue.popleft()
             visited +=1
-            for nxt in graph[node] : 
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0 :
-                    queue.append(nxt)
+            for neighbor in graph[node] : 
+                indegree[neighbor] -=1 
+                if indegree[neighbor] == 0 :
+                    queue.append(neighbor)
         return visited == numCourses
+
+                    
