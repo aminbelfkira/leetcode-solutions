ea137b2b7058ff2e1e10d0c1f49d4a2219c9aa47
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 13:04:02 +0200

    solve: 22. Generate Parentheses

--- solutions/0022-generate-parentheses/solution.py
+++ solutions/0022-generate-parentheses/solution.py
@@ -1,31 +1,29 @@
 # 22. Generate Parentheses
 # https://leetcode.com/problems/generate-parentheses/
-# Accepted: 2026-09-14T21:18:24.000Z
+# Accepted: 2026-09-21T11:03:57.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.4 MB · Beats 73.76%
-# Submission: https://leetcode.com/submissions/detail/2142009084/
+# Memory: 19.3 MB · Beats 94.63%
+# Submission: https://leetcode.com/submissions/detail/2148562461/
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis(self, n: int) -> list[str]:
+        
 
-        path = []
-        result = []
+        current = []
+        res = []
 
-        def backtrack(opened, closed) : 
-            if  closed == n : 
-                result.append("".join(path))
+        def aux(opened, closed) : 
+            if closed == n : 
+                res.append("".join(current))
                 return
-            if opened < n : 
-                path.append("(")
-                backtrack(opened+1, closed)
-                path.pop()
-            if closed < opened : 
-                path.append(')')
-                backtrack(opened, closed +1)
-                path.pop()
-        
-        backtrack(0,0)
-        return result
-
-        
+            if opened < n: 
+                current.append("(")
+                aux(opened +1, closed)
+                current.pop()
+            if closed <opened : 
+                current.append(")")
+                aux(opened, closed +1)
+                current.pop()
+        aux(0,0)
+        return res
