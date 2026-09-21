8b95c1551d7ed7ef62efea57914442ffd6c178bf
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 12:58:06 +0200

    solve: 17. Letter Combinations of a Phone Number

--- solutions/0017-letter-combinations-of-a-phone-number/solution.py
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,39 +1,36 @@
 # 17. Letter Combinations of a Phone Number
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-# Accepted: 2026-09-14T20:44:34.000Z
+# Accepted: 2026-09-21T10:58:02.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.4 MB · Beats 41.33%
-# Submission: https://leetcode.com/submissions/detail/2141994739/
+# Memory: 19.4 MB · Beats 10.32%
+# Submission: https://leetcode.com/submissions/detail/2148558033/
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        dico = {
+    def letterCombinations(self, digits: str) -> list[str]:
+        
+        letters = {
             "0" : "",
-            "1" : "",
+            "1":"",
             "2" : "abc",
-            "3": "def", 
-            "4" : "ghi",
-            "5" : "jkl",
+            "3" : "def",
+            "4" : "ghi", 
+            "5" : "jkl", 
             "6" : "mno",
             "7" : "pqrs",
             "8" : "tuv",
             "9" : "wxyz"
         }
-
-        path = []
-        result = []
-
-        def backtrack() : 
-            if len(path) == len(digits) : 
-                res = path.copy()
-
-                result.append("".join(res))
+        res = []
+        current = []
+        def aux(start) :
+            if len(current) == len(digits) :
+                res.append("".join(current))
                 return
-            n = len(path)
-            for letter in dico[digits[n]] : 
-                path.append(letter)
-                backtrack()
-                path.pop()
-        backtrack()
-        return result
+            for letter in letters[digits[start]] : 
+                current.append(letter)
+                aux(start+1)
+                current.pop()
+        aux(0)
+        return res
+            
