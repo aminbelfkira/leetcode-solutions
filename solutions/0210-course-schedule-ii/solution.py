85b42b9211b61d6298cd2a457c0c0f5a24f52109
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 09:58:22 +0200

    solve: 210. Course Schedule II

--- solutions/0210-course-schedule-ii/solution.py
+++ solutions/0210-course-schedule-ii/solution.py
@@ -1,26 +1,30 @@
 # 210. Course Schedule II
 # https://leetcode.com/problems/course-schedule-ii/
-# Accepted: 2026-09-14T20:06:16.000Z
+# Accepted: 2026-09-21T07:58:17.000Z
 # Language: Python3
-# Runtime: 8 ms · Beats 11.09%
-# Memory: 20.4 MB · Beats 75.38%
-# Submission: https://leetcode.com/submissions/detail/2141975163/
+# Runtime: 3 ms · Beats 74.88%
+# Memory: 20.4 MB · Beats 75.7%
+# Submission: https://leetcode.com/submissions/detail/2148416244/
 
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
         from collections import defaultdict, deque
-        indegree = [0] * numCourses
+
         graph = defaultdict(list)
+        indegree = [0]* numCourses
         for course, prereq in prerequisites : 
-            indegree[course] +=1
             graph[prereq].append(course)
-        queue = deque(c for c in range(numCourses) if indegree[c] == 0)
+            indegree[course]+=1
+        visited =0
         res = []
+        queue = deque(c for c in range(numCourses) if indegree[c] == 0)
+
         while queue : 
             current = queue.popleft()
             res.append(current)
+            visited +=1
             for nxt in graph[current] : 
-                indegree[nxt] -=1
-                if indegree[nxt] == 0 : 
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0 :
                     queue.append(nxt)
-        return res if len(res) == numCourses else []
+        return [] if visited != numCourses else res
