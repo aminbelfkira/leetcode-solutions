1fd3bca32ca668957708fee17491c2992579a320
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 14:53:05 +0200

    solve: 162. Find Peak Element

--- solutions/0162-find-peak-element/solution.py
+++ solutions/0162-find-peak-element/solution.py
@@ -1,17 +1,19 @@
 # 162. Find Peak Element
 # https://leetcode.com/problems/find-peak-element/
-# Accepted: 2026-09-15T15:23:21.000Z
+# Accepted: 2026-09-21T12:52:52.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.5 MB · Beats 11.31%
-# Submission: https://leetcode.com/submissions/detail/2142741130/
+# Memory: 19.3 MB · Beats 80.66%
+# Submission: https://leetcode.com/submissions/detail/2148640437/
 
 class Solution:
     def findPeakElement(self, nums: list[int]) -> int:
+        n = len(nums)
         left = 0
-        right = len(nums) - 1
+        right = n-1
+
         while left < right : 
-            mid = (left+right) //2
+            mid = (left + right)//2
             if nums[mid] < nums[mid+1] : 
                 left = mid +1
             else :
