d08d78f22d0c360de14ddab833ebf61efea163ab
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-21 12:53:34 +0200

    solve: 433. Minimum Genetic Mutation

--- solutions/0433-minimum-genetic-mutation/solution.py
+++ solutions/0433-minimum-genetic-mutation/solution.py
@@ -1,29 +1,30 @@
 # 433. Minimum Genetic Mutation
 # https://leetcode.com/problems/minimum-genetic-mutation/
-# Accepted: 2026-09-14T20:31:56.000Z
+# Accepted: 2026-09-21T10:53:30.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.5 MB · Beats 11.45%
-# Submission: https://leetcode.com/submissions/detail/2141988701/
+# Memory: 19.4 MB · Beats 41.95%
+# Submission: https://leetcode.com/submissions/detail/2148554553/
 
 class Solution:
-    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+    def minMutation(self, startGene: str, endGene: str, bank: list[str]) -> int:
         
-        if endGene not in bank :
+        if endGene not in bank : 
             return -1
-        remaining = set(bank)
+
         from collections import deque
         queue = deque([(startGene, 0)])
+        seen = set()
         while queue : 
             current, mutations = queue.popleft()
+            seen.add(current)
             if current == endGene : 
                 return mutations
             for i, base in enumerate(current) : 
-
-                for gene in "ACGT" : 
-                    if gene != base : 
-                        new_gene = current[:i] + gene + current[i+1:]
-                        if new_gene in remaining : 
-                            queue.append((new_gene,mutations +1))
-                            remaining.remove(new_gene)
+                for new_gene in 'ACGT' : 
+                    if new_gene != base : 
+                        modified = current[:i] + new_gene + current[i+1 :]
+                        if modified not in seen and modified in bank: 
+                            queue.append((modified, mutations +1))
         return -1
+        
