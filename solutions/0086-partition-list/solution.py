03de5dc8d87ecce0ca158d22442468004362f94c
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-22 08:46:19 +0200

    solve: 86. Partition List

--- solutions/0086-partition-list/solution.py
+++ solutions/0086-partition-list/solution.py
@@ -1,10 +1,10 @@
 # 86. Partition List
 # https://leetcode.com/problems/partition-list/
-# Accepted: 2026-09-11T20:57:48.000Z
+# Accepted: 2026-09-22T06:46:14.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.3 MB · Beats 39.32%
-# Submission: https://leetcode.com/submissions/detail/2138967958/
+# Memory: 19.3 MB · Beats 39.8%
+# Submission: https://leetcode.com/submissions/detail/2149425135/
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -12,20 +12,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-
-        before_dummy = ListNode()
-        after_dummy = ListNode()
+    def partition(self, head: ListNode | None, x: int) -> ListNode | None:
+        
+        before_dummy = ListNode(0)
+        after_dummy = ListNode(0)
         before = before_dummy
         after = after_dummy
-
         current = head 
         while current : 
             if current.val < x : 
                 before.next = current
                 before = before.next
             else : 
-                after.next = current
+                after.next =current
                 after = after.next
             current = current.next
         after.next = None
