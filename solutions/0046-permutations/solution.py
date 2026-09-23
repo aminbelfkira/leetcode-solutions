204e8ba9e84edfea65262078b8fb6fa5282d45ed
aminbelfkira <103946212+aminbelfkira@users.noreply.github.com>
2026-09-23 11:06:44 +0200

    solve: 46. Permutations

--- solutions/0046-permutations/solution.py
+++ solutions/0046-permutations/solution.py
@@ -1,24 +1,24 @@
 # 46. Permutations
 # https://leetcode.com/problems/permutations/
-# Accepted: 2026-09-14T21:10:16.000Z
+# Accepted: 2026-09-23T09:06:38.000Z
 # Language: Python3
 # Runtime: 0 ms · Beats 100%
-# Memory: 19.4 MB · Beats 87.84%
-# Submission: https://leetcode.com/submissions/detail/2142005806/
+# Memory: 19.5 MB · Beats 36.74%
+# Submission: https://leetcode.com/submissions/detail/2150691698/
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        path = []
-        result = []
-
-        def backtrack() : 
-            if len(path)==len(nums) : 
-                result.append(path.copy())
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        
+        current = []
+        res = [] 
+        def aux() :
+            if len(current) == len(nums) : 
+                res.append(current.copy())
                 return
             for num in nums : 
-                if num not in path :
-                    path.append(num)
-                    backtrack()
-                    path.pop()
-        backtrack()
-        return result
+                if num not in current :
+                    current.append(num)
+                    aux()
+                    current.pop()
+        aux()
+        return res
